ade245d4cdcdccf2fccf2082563031f6b20d8050
jeanmarielepioufle <jm@nexmodeling.com>
2016-11-18 14:25:35 +0100

    typo correction

--- R/getPrecipLZ.R
+++ R/getPrecipLZ.R
@@ -1,7 +1,7 @@
 #' Get the observations
 #'
 #' Get and process, if needed, the observations
-#' @param method method for processing observations, "none", "lz"
+#' @param method method for processing observations, "lz", "processedNVE"
 #' @param path Directory where to get the files
 #' @param filename Filename (path included) of the file to be read
 #' @param modelPrecipLZ  list(nbLevelZone,Plr,hfelt,midmetp)
@@ -14,7 +14,7 @@
 getPrecipLZ <- function(method=NULL,path=NULL,filename=NULL,modelPrecipLZ=NULL) {
 
     precipLZ <- switch(method,
-                   "none"        = getPrecipLZ.none(path=path,filename=filename),
+                   "processedNVE"        = getPrecipLZ.processedNVE(path=path,filename=filename),
                    "lz"          = getPrecipLZ.lz(path=path,filename=filename,modelPrecipLZ=modelPrecipLZ),
                    (message=paste0("Invalid method:", method,".")))
 
@@ -22,7 +22,7 @@ getPrecipLZ <- function(method=NULL,path=NULL,filename=NULL,modelPrecipLZ=NULL) 
 }
 
 
-getPrecipLZ.none <- function(path,filename){
+getPrecipLZ.processedNVE <- function(path,filename){
   env <- environment()
   path <- normalizePath(file.path(path,filename),mustWork = FALSE)
   tmp <- utils::read.table(path,sep="\t")
@@ -38,7 +38,6 @@ getPrecipLZ.lz <- function(path,filename,modelPrecipLZ){
   tmp <- utils::read.table(path,sep="\t")
   assign("precip",tmp[,5],envir=env)
   rm(tmp)
-  get("precip",envir=env)
-  precipLZ <- precip*(1.0+modelPrecipLZ$Plr*((modelPrecipLZ$hfelt-modelPrecipLZ$midmetp)/100.0))
+  precipLZ <- get("precip",envir=env)*(1.0+modelPrecipLZ$Plr*((modelPrecipLZ$hfelt-modelPrecipLZ$midmetp)/100.0))
   return(precipLZ)
 }
